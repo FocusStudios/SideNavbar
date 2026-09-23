7155b080e48f07bd19bd60c06000b6397bacf361
Focus <focusstudios2025@gmail.com>
2026-09-23 22:41:45 +0300

    Update GlowEffect.js

--- JS/GlowEffect.js
+++ JS/GlowEffect.js
@@ -5,13 +5,6 @@ const Body = document.querySelector(".Body");
 
 const mainFrames = ["Home", "Community", "Email", "Share", "Settings", "Support", "Log Out"]
 
-const Body = document.querySelector(".Body");
-
-
-// Create Main Frames & Subframes
-
-const mainFrames = ["Home", "Community", "Email", "Share", "Settings", "Support", "Log Out"]
-
 const subFrames = ["Recent", "Adobe XD", "Figma", "Sketch", "Frames", "Icons", "Pallets", "Vectors"]
 
 
@@ -296,17 +289,6 @@ const Save = document.querySelector(".account-settings .save");
 const CamerInput = document.querySelector("#CamerInput");
 const Sections = document.querySelectorAll(".account-settings .fields section");
 
-
-if(window.innerHeight > Body.offsetHeight){
-
-Body.style.position = "relative";
-
-}else{
-
-Body.style.position = "absolute";
-Body.style.top = "0";
-
-}
 
 //Activate & Deactivate Side Navbar
 
